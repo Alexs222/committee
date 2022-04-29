8b2bb8fa9e3485ba79d75025baad508fbb2d0acf
Alexs222 <aleks.nikulin@gmail.com>
2022-04-29 18:40:30 +0200

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./app.scss";
+
+export default class App extends Component {
+
+  render() {
+
+    return (
+      <div className="todo-app">
+        Start project
+      </div>
+    );
+  }
+}
