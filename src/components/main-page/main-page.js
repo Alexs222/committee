9264ee7842e573f57f85094c0e10ffbf36ec4445
Alexander Nikulin <aleks.nikulin@gmail.com>
2022-06-01 00:00:04 +0300

    main-page background test

--- src/components/main-page/main-page.js
+++ src/components/main-page/main-page.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 
 import "./main-page.scss";
 
+import background from './building.jpg'
+
 export default class MainPage extends Component {
 
   render() {
 
     return (
-      <div className="page main-page">
+      <div className="page main-page" style={{ backgroundImage: `url(${background})` }}>
         Main page
       </div>
     );
